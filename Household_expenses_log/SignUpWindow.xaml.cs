14f05c289490c521c5a04bfbf21199cead1df653
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2020-06-14 15:04:06 +0300

    Commit #2
    -Added warnings on Sign up window
    -Added PasswordBoxes

--- Household_expenses_log/SignUpWindow.xaml.cs
+++ Household_expenses_log/SignUpWindow.xaml.cs
@@ -7,10 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using Label = System.Windows.Controls.Label;
 
 namespace Household_expenses_log
 {
@@ -20,26 +23,60 @@ namespace Household_expenses_log
     public partial class SignUpWindow : Window
     {
         private MainWindow _previous_window;
+
         public SignUpWindow(MainWindow previous_window)
         {
             InitializeComponent();
             _previous_window = previous_window;
         }
 
+        //Метод, очищающий все поля в окне
         public void ClearAllFields()
-        { 
-            
+        {
+            tb_users_name.Clear();
+            tb_email.Clear();
+            pb_pass.Clear();
+            pb_pass_repeat.Clear();
         }
 
         private void SignUpWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            MessageBoxResult dialog_result = MessageBox.Show("Закрыть приложение?", "Завершение работы", MessageBoxButton.YesNo);
+            MessageBoxResult dialog_result = System.Windows.MessageBox.Show("Закрыть приложение?", "Завершение работы", MessageBoxButton.YesNo);
 
             if (dialog_result == MessageBoxResult.Yes)
-                Application.Current.Shutdown(); //Завершаем работу приложения
+                System.Windows.Application.Current.Shutdown(); //Завершаем работу приложения
             else
             {
                 e.Cancel = true;
+            }
+        }
+
+        //Метод, проверяющий, не содержит ли поле с именем пользователя посторонних символов
+        private void tb_users_name_LostFocus(object sender, RoutedEventArgs e)
+        {
+            foreach (char l in tb_users_name.Text)
+            {
+                if (!char.IsLetter(l))
+                {
+                    //Делаем видимой предупреждающую надпись, если встретили не букву
+                    lb_warning1.Visibility = Visibility.Visible;
+                    return;
+                }
+            }
+            //Если были введены только буквы, скрываем предупреждение
+            lb_warning1.Visibility = Visibility.Hidden;
+        }
+
+        private void tb_email_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //Проверка корректности email
+            string email_pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+
+            if (!Regex.IsMatch(tb_email.Text, email_pattern, RegexOptions.IgnoreCase))
+            {
+                
+                return;
             }
         }
     }
