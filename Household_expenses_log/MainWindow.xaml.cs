ec5974568233c35aa63f15a74f38ce6281d71712
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2020-06-14 11:56:56 +0300

    Initial commit
    Login window was created

---
+++ Household_expenses_log/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Household_expenses_log
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SignUpWindow sign_up_window;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sign_up_window = new SignUpWindow(this);
+        }
+
+        private void b_sign_up_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            //Очищаем поля в окне регистрации, если они были заполнены до этого
+            sign_up_window.ClearAllFields();
+            sign_up_window.Show();
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult dialog_result = MessageBox.Show("Закрыть приложение?", "Завершение работы", MessageBoxButton.YesNo);
+
+            if (dialog_result == MessageBoxResult.Yes)
+                Application.Current.Shutdown(); //Завершаем работу приложения
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ Household_expenses_log/SignUpWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Household_expenses_log
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+        private MainWindow _previous_window;
+        public SignUpWindow(MainWindow previous_window)
+        {
+            InitializeComponent();
+            _previous_window = previous_window;
+        }
+
+        public void ClearAllFields()
+        { 
+            
+        }
+
+        private void SignUpWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult dialog_result = MessageBox.Show("Закрыть приложение?", "Завершение работы", MessageBoxButton.YesNo);
+
+            if (dialog_result == MessageBoxResult.Yes)
+                Application.Current.Shutdown(); //Завершаем работу приложения
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
